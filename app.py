71ca3be2227a246e6519a9fb7c73f2a1a1df3aa7
“juanpi19” <juanpablacho19@gmail.com>
2025-02-13 17:14:43 -0800

    changing flask code

--- app.py
+++ app.py
@@ -4,12 +4,6 @@ import os
 
 app = Flask(__name__)
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Access the variables
-api_key = os.getenv("secret_key")
-
 @app.route('/')
 def index():
     return render_template('ecommerce-site.html')
