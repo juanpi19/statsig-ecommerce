7086a364a6f7e114b0858281522c46a8ece5d08a
“juanpi19” <juanpablacho19@gmail.com>
2025-02-13 18:52:02 -0800

    changes

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, jsonify, request, session
 from dotenv import load_dotenv
 import os
 
+load_dotenv() 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('ecommerce-site.html')
+    statsig_key = os.environ.get('STATSIG_CLIENT_KEY', 'default_key')
+    return render_template('ecommerce-site.html', statsig_key=statsig_key)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
