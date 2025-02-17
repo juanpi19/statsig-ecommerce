df32e3af4463103f917ecc11a2b5c94c86653d37
“juanpi19” <juanpablacho19@gmail.com>
2025-02-16 20:13:14 -0800

    changing html code

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # statsig_key = os.environ.get('STATSIG_CLIENT_KEY', 'default_key')
+    return render_template('ecommerce-site.html')
+
+@app.route('/get-statsig-key')
+def get_statsig_key():
     statsig_key = os.environ.get('STATSIG_CLIENT_KEY', 'default_key')
-    return render_template('ecommerce-site.html', statsig_key=statsig_key)
+    return jsonify({'key': statsig_key})
 
 
 if __name__ == '__main__':
